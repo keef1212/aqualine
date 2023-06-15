45673690d3fc7fabd956bde2bfe7e54a8bdb25ba
keef <134746972+keef1212@users.noreply.github.com>
2023-06-15 21:08:54 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,48 @@
+import socket
+import threading
+import os
+import sys
+
+
+class ChatClient:
+    def __init__(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_host = input("Enter the server IP address: ")
+        self.server_port = int(input("Enter the server port: "))
+        self.nickname = input("Enter your nickname: ")
+
+    def connect(self):
+        try:
+            self.client_socket.connect((self.server_host, self.server_port))
+        except ConnectionRefusedError:
+            print("Connection refused. The server is not available.")
+            sys.exit(1)
+
+        self.send_message(f"{self.nickname}")
+
+        receive_thread = threading.Thread(target=self.receive_messages)
+        receive_thread.start()
+
+        while True:
+            message = input()
+            if message == "/quit":
+                self.send_message(message)
+                break
+            self.send_message(f"{self.nickname}: {message}")
+
+    def send_message(self, message):
+        self.client_socket.send(message.encode())
+
+    def receive_messages(self):
+        while True:
+            try:
+                message = self.client_socket.recv(1024).decode()
+                print(message)
+            except ConnectionResetError:
+                print("Disconnected from the server.")
+                sys.exit(1)
+
+
+client = ChatClient()
+client.connect()
---
+++ server.py
@@ -0,0 +1,68 @@
+import socket
+import threading
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+class ChatServer:
+    def __init__(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_host = '127.0.0.1'
+        self.server_port = 8000
+        self.client_sockets = []
+        self.client_addresses = []
+        self.nicknames = {}
+
+    def start(self):
+        self.server_socket.bind((self.server_host, self.server_port))
+        self.server_socket.listen(5)
+        print("Server started. Listening for connections...")
+
+        while True:
+            client_socket, client_address = self.server_socket.accept()
+            self.client_sockets.append(client_socket)
+            self.client_addresses.append(client_address)
+            threading.Thread(target=self.handle_client, args=(client_socket, client_address)).start()
+
+    def handle_client(self, client_socket, client_address):
+        nickname = self.receive_message(client_socket)
+        self.nicknames[client_socket] = nickname
+
+        print(f"New connection from {client_address[0]}:{client_address[1]} (Username: {nickname})")
+        self.broadcast_message(f"{nickname} has joined the chat!")
+
+        try:
+            while True:
+                message = self.receive_message(client_socket)
+                if not message:
+                    break
+                self.broadcast_message(message, sender_socket=client_socket, sender_nickname=nickname)
+        except ConnectionResetError:
+            pass
+
+        self.client_sockets.remove(client_socket)
+        self.client_addresses.remove(client_address)
+        self.nicknames.pop(client_socket)
+        client_socket.close()
+        print(f"Connection closed: {client_address[0]}:{client_address[1]} (Username: {nickname})")
+        self.broadcast_message(f"{nickname} has left the chat.")
+
+    def receive_message(self, client_socket):
+        try:
+            message = client_socket.recv(1024).decode()
+            return message
+        except ConnectionResetError:
+            pass
+
+    def broadcast_message(self, message, sender_socket=None, sender_nickname=None):
+        for client_socket in self.client_sockets:
+            if sender_nickname and client_socket == sender_socket:
+                client_socket.send(message.encode())
+            elif sender_nickname:
+                client_socket.send(f"{sender_nickname}: {message}".encode())
+            else:
+                client_socket.send(message.encode())
+
+
+server = ChatServer()
+server.start()

5b4b8e2eeda581b91c41b4a66202883a3244c73e
ifindu19 <134746972+keef1212@users.noreply.github.com>
2023-06-16 18:37:47 +0100

    Added admin commands

--- server.py
+++ server.py
@@ -7,11 +7,12 @@ os.system('cls' if os.name == 'nt' else 'clear')
 class AquaServer:
     def __init__(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_host = '127.0.0.1' #If you want this to work across networks, make sure you replace "127.0.0.1" with the local IP of your machine (eg: 192.168.0.40)
+        self.server_host = '127.0.0.1'
         self.server_port = 8000
         self.client_sockets = []
         self.client_addresses = []
         self.nicknames = {}
+        self.admins = {'admin_ip_address'}  # Set the IP address of the admin here
 
     def start(self):
         self.server_socket.bind((self.server_host, self.server_port))
@@ -29,9 +30,10 @@ class AquaServer:
         self.nicknames[client_socket] = nickname
 
         print(f"New connection from {client_address[0]}:{client_address[1]} (Username: {nickname})")
-        if nickname == "badactor":
+        self.broadcast_message(f"{nickname} has joined the chat!")
+        if nickname == "badactor": # This is just an example, I recommend you use client_address instead of the nickname.
             print("WARNING: User above is listed as dangerous, be wary!")
-        if nickname == "admin": # This and badactor are just examples, I recommend you use client_address instead of the nickname.
+        if client_address == self.admins:
             self.broadcast_message(f"Administrator", sender_socket=client_socket)
         self.broadcast_message(f"{nickname} has joined the chat!", sender_socket=client_socket)
 
@@ -40,7 +42,10 @@ class AquaServer:
                 message = self.receive_message(client_socket)
                 if not message:
                     break
-                self.broadcast_message(f"{nickname}: {message}", sender_socket=client_socket)
+                if client_address[0] in self.admins:
+                    self.process_admin_command(message, client_address[0])
+                else:
+                    self.broadcast_message(message, sender_socket=client_socket, sender_nickname=nickname)
         except ConnectionResetError:
             pass
 
@@ -58,11 +63,36 @@ class AquaServer:
         except ConnectionResetError:
             pass
 
-    def broadcast_message(self, message, sender_socket=None):
+    def broadcast_message(self, message, sender_socket=None, sender_nickname=None):
         for client_socket in self.client_sockets:
-            if client_socket != sender_socket:
+            if sender_nickname and client_socket == sender_socket:
+                client_socket.send(message.encode())
+            elif sender_nickname:
+                client_socket.send(f"{sender_nickname}: {message}".encode())
+            else:
                 client_socket.send(message.encode())
 
+    def process_admin_command(self, message, admin_ip):
+        command_parts = message.strip().split()
+        if len(command_parts) > 0:
+            command = command_parts[0]
+            if command == '/ban':
+                if len(command_parts) > 1:
+                    ip_to_ban = command_parts[1]
+                    self.ban_ip(ip_to_ban)
+                    print(f"Admin {admin_ip} banned IP: {ip_to_ban}")
+            elif command == '/unban':
+                if len(command_parts) > 1:
+                    ip_to_unban = command_parts[1]
+                    self.unban_ip(ip_to_unban)
+                    print(f"Admin {admin_ip} unbanned IP: {ip_to_unban}")
+
+    def ban_ip(self, ip_address):
+        self.client_addresses = [addr for addr in self.client_addresses if addr[0] != ip_address]
+
+    def unban_ip(self, ip_address):
+        if ip_address not in [addr[0] for addr in self.client_addresses]:
+            self.client_addresses.append((ip_address, 0))  # Add the IP back to the list
 
 server = AquaServer()
 server.start()

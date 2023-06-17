b7907202795011a84111261c7898ac61fafc8014
ifindu19 <134746972+keef1212@users.noreply.github.com>
2023-06-17 16:55:16 +0100

    Update server.py

--- server/server.py
+++ server/server.py
@@ -28,6 +28,12 @@ class AquaServer:
 
     def handle_client(self, client_socket, client_address):
         nickname = self.receive_message(client_socket)
+
+        if self.is_username_taken(nickname):
+            self.send_message("Username is already in use. Please choose a different username.", client_socket)
+            client_socket.close()
+            return
+
         self.nicknames[client_socket] = nickname
 
         print(f"New connection from {client_address[0]}:{client_address[1]} (Username: {nickname})")
@@ -41,9 +47,9 @@ class AquaServer:
                 elif message.startswith("/private"):
                     self.send_private_message(message, sender_socket=client_socket)
                 elif message == "/users":
-                    self.send_message(f"Connected Users: {', '.join(self.nicknames.values())}", client_socket.send)
+                    self.send_connected_users(client_socket)
                 elif message == "/help":
-                    self.send_message("Available commands: /users, /private", client_socket.send)
+                    self.send_message("Available commands: /users, /private", client_socket)
                 else:
                     self.broadcast_message(message, sender_socket=client_socket, sender_nickname=nickname)
         except ConnectionResetError:
@@ -69,11 +75,15 @@ class AquaServer:
     def broadcast_message(self, message, sender_socket=None, sender_nickname=None):
         for client_socket in self.client_sockets:
             if sender_nickname and client_socket == sender_socket:
-                self.send_message(client_socket, message)
+                self.send_message(message, client_socket)
             elif sender_nickname:
-                self.send_message(client_socket, f"{sender_nickname}: {message}")
+                self.send_message(f"{sender_nickname}: {message}", client_socket)
             else:
-                self.send_message(client_socket, message)
+                try:
+                    self.send_message(message, client_socket)
+                except OSError:
+                    print("Error sending message to a client.")
+
 
     def send_private_message(self, message, sender_socket):
         try:
@@ -84,18 +94,30 @@ class AquaServer:
                     recipient_socket = client_socket
                     break
             if recipient_socket:
-                self.send_message(recipient_socket, f"[Private] {self.nicknames[sender_socket]}: {content}")
-                self.send_message(sender_socket, f"[Private] To {self.nicknames[recipient_socket]}: {content}")
+                self.send_message(f"[Private] {self.nicknames[sender_socket]}: {content}", recipient_socket)
+                self.send_message(f"[Private] To {self.nicknames[recipient_socket]}: {content}", sender_socket)
             else:
-                self.send_message(sender_socket, f"User '{recipient}' not found.")
+                self.send_message(f"User '{recipient}' not found.", sender_socket)
         except ValueError:
-            self.send_message(sender_socket, "Invalid private message format.")
+            self.send_message("Invalid private message format.", sender_socket)
+
+    def send_connected_users(self, client_socket):
+        connected_users = ", ".join(self.nicknames.values())
+        self.send_message(f"Connected Users: {connected_users}", client_socket)
+
+    def is_username_taken(self, nickname):
+        return nickname in self.nicknames.values()
 
     def handle_server_input(self):
         while True:
-            command = input("Server Command: ")
+            command = input()
             if command == "/quit":
-                self.broadcast_message("Server is shutting down...")
+                self.broadcast_message("Server closed.")
+                os._exit(0)
+            elif command == "/restart":
+                self.broadcast_message("Server is restarting.")
+                print("Note: the server wont restart on its own, you have to reopen the server file.")
+                print("This should only be used if you want to alert people that your restarting and not closing.")
                 os._exit(0)
             elif command.startswith("/servermsg"):
                 server_message = command.split(" ", 1)[1]
